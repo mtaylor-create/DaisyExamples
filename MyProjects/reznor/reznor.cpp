bf8133075392ca84dd766cfbfa6193160b9b544e
Mike Taylor <mdtaylor@andrew.cmu.edu>
2022-10-16 16:05:59 -0400

    Figured out i2c address changes

--- MyProjects/reznor/reznor.cpp
+++ MyProjects/reznor/reznor.cpp
@@ -30,6 +30,21 @@ float crushsl, crushsr;
 int   panelInputA;
 int   lastUpdate;
 
+// struct Config
+//     {
+//         I2CHandle::Config i2c_config;
+//         uint8_t           i2c_address;
+//         void              Defaults()
+//         {
+//             i2c_config.periph         = I2CHandle::Config::Peripheral::I2C_1;
+//             i2c_config.speed          = I2CHandle::Config::Speed::I2C_1MHZ;
+//             i2c_config.mode           = I2CHandle::Config::Mode::I2C_MASTER;
+//             i2c_config.pin_config.scl = {DSY_GPIOB, 8};
+//             i2c_config.pin_config.sda = {DSY_GPIOB, 9};
+//             i2c_address               = 0x27;
+//         }
+//     };
+
 void ConditionalParameter(float  oldVal,
                           float  newVal,
                           float &param,
@@ -155,7 +170,14 @@ int main(void)
     rev.SetLpFreq(18000.0f);
     rev.SetFeedback(0.85f);
 
-    mcp.Init();
+    Mcp23017::Config mcpt;
+    mcpt.transport_config.Defaults();
+    mcpt.transport_config.i2c_address = 0b100111;
+    //Mcp23017Transport::Config config;
+    //config.Defaults();
+    mcp.Init(mcpt);
+
+    //mcp.Init();
     mcp.PortMode(MCPPort::A, 0xFF, 0xFF, 0xFF);
     mcp.PortMode(MCPPort::B, 0xFF, 0xFF, 0xFF);
     //while(1) {
